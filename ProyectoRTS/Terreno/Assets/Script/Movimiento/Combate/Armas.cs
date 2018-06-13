a962173b3e4132f68fbbfdf8cea1cf4effbc0135
Cristian Camilo Buitrago <ccbuitragom@gmail.com>
2018-06-13 16:34:53 -0500

    preparacion arquero
    se prepara el arquero

--- ProyectoRTS/Terreno/Assets/Script/Movimiento/Combate/Armas.cs
+++ ProyectoRTS/Terreno/Assets/Script/Movimiento/Combate/Armas.cs
@@ -3,14 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Armas : MonoBehaviour {
+	public int daño=10;
+	public float tiempoRecarga = 0.5f;
 
-	// Use this for initialization
-	void Start () {
-		
+	public Transform UbicacionDisparo;
+	public GameObject FlechaPrefab;
+
+	private float Tiempo;
+	private Unidad unidad;
+
+	public bool isRedy{ get; private set;}
+
+	void Start ()
+	{
+		this.Tiempo=0;
+		this.isRedy=true;
+
+		this.unidad= GetComponent<Unidad>();
 	}
+
+	void Update(){
+		if(this.isRedy){
+			this.Tiempo = Time.deltaTime;
+			if(this.Tiempo>this.tiempoRecarga){
+				this.isRedy = true;
+				this.Tiempo = 0;
+			}
+		}
+	}
+
+	public void dispara(){
+		this.isRedy = false;
+		GameObject p = Instantiate (
+			this.FlechaPrefab,
+			this.UbicacionDisparo.position,
+			this.UbicacionDisparo.rotation
+		);
 	
-	// Update is called once per frame
-	void Update () {
-		
+		p.GetComponent<Proyectil>().arma = this;
+	}
+
+	public void OnProyectilCollision(Proyectil flecha,Unidad unidad){
+		if(unidad.faccion != this.unidad.faccion){
+			unidad.GetComponent<Salud> ().salud -= this.daño;
+			Destroy (flecha);
+		}	
 	}
 }
--- ProyectoRTS/Terreno/Assets/Script/Movimiento/Combate/Proyectil.cs
+++ ProyectoRTS/Terreno/Assets/Script/Movimiento/Combate/Proyectil.cs
@@ -10,11 +10,19 @@ public class Proyectil : MonoBehaviour {
 	public Armas arma;
 	public float duracion = 10;
 
-//	void Start(){
-//		Destroy (this.gameObject,this.duracion);
-//		GetComponent<Rigidbody> ().velocity- this.transform.forward * this.velocidad;
-//	}
+	void Start(){
+		Destroy (this.gameObject,this.duracion);
+		GetComponent<Rigidbody> ().velocity= this.transform.forward * this.velocidad;
+	}
 
-//	void OnCollisionEnter(){
-//	}
+	void OnCollisionEnter(){
+		Unidad u = gameObject.GetComponent<Unidad> ();
+		if (u != null) {
+			this.arma.OnProyectilCollision (this,u);
+		} else {
+			Destroy (this.gameObject);
+		
+		}
+			
+	}
 }
