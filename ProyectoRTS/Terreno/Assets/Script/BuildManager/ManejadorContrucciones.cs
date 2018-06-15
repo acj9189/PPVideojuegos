b7f61cb541c9d7a90a7188aec089c5758b34c6e0
acj9189 <acj8991@gmail.com>
2018-06-15 03:41:00 -0500

    Actualizacion Creacion de Objetos

--- ProyectoRTS/Terreno/Assets/Script/BuildManager/ManejadorContrucciones.cs
+++ ProyectoRTS/Terreno/Assets/Script/BuildManager/ManejadorContrucciones.cs
@@ -6,43 +6,54 @@ using System.Collections.Generic;
 
 public class ManejadorContrucciones : MonoBehaviour {
 
-    private int tipoObjeyo;
+    private int tipoObjetoAConstruir;
 
 
     public GameObject[] edificiosPrefabricados;
     public Camera mainCamera;
     public LayerMask layerWhereClick;
+
+    private int rayDistance = 100000;
 	// Use this for initialization
 	void Start () {
-        this.tipoObjeyo = 0;
+        this.tipoObjetoAConstruir = 0;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if(this.tipoObjeyo != 0) {
+        if(this.tipoObjetoAConstruir != 0) {
             if (Input.GetButtonDown("Fire1")){
 
                 Ray ray = this.mainCamera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit info; // punto de choque con el rayo
+                if (Physics.Raycast(ray, out info, this.rayDistance, this.layerWhereClick)) {
 
-                switch (this.tipoObjeyo)
-                {
-                    case 1:
-                        break;
-                    case 2:
-                        break;
+                    switch (this.tipoObjetoAConstruir)
+                    {
+                        case 1:
+                            Instantiate(this.edificiosPrefabricados[0], info.point, Quaternion.identity);
+                            break;
+                        case 2:
+                            Instantiate(this.edificiosPrefabricados[1], info.point, Quaternion.identity);
+                            break;
+
+                    }
+                    this.tipoObjetoAConstruir = 0;
 
                 }
-                this.tipoObjeyo = 0;
+
+               
 
             }
         }
 		
 	}
 
-    public void seleccionObjetoEdificio(int tipoEdificio)
-    {
+    public void seleccionObjetoEdificio(int tipoEdificio){
+
+        this.tipoObjetoAConstruir= tipoEdificio;
 
     }
 }
