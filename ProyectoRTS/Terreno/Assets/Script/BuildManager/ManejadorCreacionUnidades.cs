a743486691ec8e3e0aca68ad23407b15f71802ee
acj9189 <acj8991@gmail.com>
2018-06-15 04:15:33 -0500

    Ultima Actualizacion

---
+++ ProyectoRTS/Terreno/Assets/Script/BuildManager/ManejadorCreacionUnidades.cs
@@ -0,0 +1,67 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ManejadorCreacionUnidades : MonoBehaviour
+{
+
+    private int tipoUnidadACrear;
+
+
+    public GameObject[] UnidadesGuerrerosPrefabricados;
+    public Camera mainCamera;
+    public LayerMask layerWhereClick;
+
+    private int rayDistance = 100000;
+    // Use this for initialization
+    void Start()
+    {
+        //hh
+        this.tipoUnidadACrear = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (this.tipoUnidadACrear != 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+
+                Ray ray = this.mainCamera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit info; // punto de choque con el rayo
+                if (Physics.Raycast(ray, out info, this.rayDistance, this.layerWhereClick))
+                {
+
+                    switch (this.tipoUnidadACrear)
+                    {
+                        case 1:
+                            Instantiate(this.UnidadesGuerrerosPrefabricados[0], info.point, Quaternion.identity);
+                            break;
+                        case 2:
+                            Instantiate(this.UnidadesGuerrerosPrefabricados[1], info.point, Quaternion.identity);
+                            break;
+
+                    }
+                    this.tipoUnidadACrear = 0;
+
+                }
+
+
+
+            }
+        }
+
+    }
+
+    public void seleccionTipoUnidad(int tipoUnidad)
+    {
+
+        this.tipoUnidadACrear = tipoUnidad;
+
+    }
+}
