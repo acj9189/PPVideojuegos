a1b608620cbf178dc5211f9070685e5c35643ea6
acj9189 <acj8991@gmail.com>
2018-06-15 02:35:14 -0500

    Crear Objetos a poner

---
+++ ProyectoRTS/Terreno/Assets/Script/BuildManager/ManejadorContrucciones.cs
@@ -0,0 +1,48 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ManejadorContrucciones : MonoBehaviour {
+
+    private int tipoObjeyo;
+
+
+    public GameObject[] edificiosPrefabricados;
+    public Camera mainCamera;
+    public LayerMask layerWhereClick;
+	// Use this for initialization
+	void Start () {
+        this.tipoObjeyo = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(this.tipoObjeyo != 0) {
+            if (Input.GetButtonDown("Fire1")){
+
+                Ray ray = this.mainCamera.ScreenPointToRay(Input.mousePosition);
+
+                switch (this.tipoObjeyo)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+
+                }
+                this.tipoObjeyo = 0;
+
+            }
+        }
+		
+	}
+
+    public void seleccionObjetoEdificio(int tipoEdificio)
+    {
+
+    }
+}
