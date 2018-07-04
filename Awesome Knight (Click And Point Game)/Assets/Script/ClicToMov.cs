e3045c911f777da62906eae1ad1183405fb94fbc
acj9189 <acj8991@gmail.com>
2018-07-03 21:10:43 -0500

    Actualizacion Juego y prefabs....

---
+++ Awesome Knight (Click And Point Game)/Assets/Script/ClicToMov.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClicToMov : MonoBehaviour {
+
+
+/*	private NavMeshAgent jugador;
+	public Camera Camara;
+	private int DistanciaRayo;
+	private bool Atacar;
+	private Armas arma;
+	private MovTester Enemigo;
+	private Vector3 Objetivo;
+
+	private int PuntosAtaque;
+	private int PuntosDefensa;
+
+	// Use this for initialization
+	void Start () {
+		this.jugador=GetComponent<NavMeshAgent> ();
+		this.DistanciaRayo = 200;
+		this.Atacar = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(this.transform.position,this.Objetivo)<=0.5){
+
+			this.Objetivo = this.transform.position;
+			this.jugador.destination = Objetivo;
+			this.GetComponent<Animator> ().SetInteger("Caminar",0);
+			this.GetComponent<Animator> ().SetInteger("Correr",1);
+			if(Atacar){
+				this.GetComponent<Animator> ().SetInteger("Pelear",1);
+			}
+			
+		}
+
+
+		if(Input.GetMouseButtonDown(0)){
+			Ray Rayo = this.Camara.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (Rayo, out hit, DistanciaRayo)) {
+				GameObject PosEnemigo = hit.collider.gameObject;
+				this.Enemigo=PosEnemigo.GetComponent<MovTester>();
+				if (Enemigo == null) {
+					Debug.Log (hit.point);
+					this.Objetivo = hit.point;
+					this.GetComponent<Animator> ().SetInteger("Caminar",1);
+					OrdenMover (hit.point);
+
+					this.Atacar = false;
+
+				} else {
+					this.Objetivo = hit.point;
+					this.GetComponent<Animator> ().SetInteger("Correr",1);
+					OrdenMover (Enemigo.transform.position);
+					this.Atacar = true;
+				}
+			}
+		}
+	}
+
+	public void OrdenMover(Vector3 Punto){
+		this.jugador.destination = Punto;
+	}*/
+}
