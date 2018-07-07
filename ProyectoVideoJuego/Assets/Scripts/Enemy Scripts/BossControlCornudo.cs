f3da2e0a172800b390933c7f6a3c26dc10088f58
acj9189 <acj8991@gmail.com>
2018-07-06 21:27:15 -0500

    Enemigo.... Funciona parcial

---
+++ ProyectoVideoJuego/Assets/Scripts/Enemy Scripts/BossControlCornudo.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossControlCornudo : MonoBehaviour {
+
+	private Transform playerTarget;
+	private BossStateChecker bossStateChecker;
+	private NavMeshAgent navAgent;
+	private Animator anim;
+
+	private bool finishedAttacking = true;
+	private float currentAttackTime;
+	private float waitAttackTime = 1f;
+
+	void Awake () {
+		playerTarget = GameObject.FindGameObjectWithTag ("Player").transform;
+		bossStateChecker = GetComponent<BossStateChecker> ();
+		navAgent = GetComponent<NavMeshAgent> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update () {
+
+		if (finishedAttacking) {
+			GetStateControl();
+		} else {
+
+			anim.SetInteger ("Transicion", 3);
+
+			if (!anim.IsInTransition (0) && anim.GetCurrentAnimatorStateInfo (0).IsName ("GolpeaDer")) {
+				finishedAttacking = true;
+			}
+		}
+	}
+
+	void GetStateControl() {
+		if (bossStateChecker.BossState == Boss_State.DEATH) {
+
+			navAgent.isStopped = true;
+
+			anim.SetInteger ("Transicion", 4);
+
+			Destroy (gameObject, 3f);
+
+		} else {
+
+			if (bossStateChecker.BossState == Boss_State.PAUSE) {
+
+				navAgent.isStopped = false;
+				anim.SetInteger ("Transicion", 1);
+
+				navAgent.SetDestination (playerTarget.position);
+
+			} else if (bossStateChecker.BossState == Boss_State.ATTACK) {
+
+				//anim.SetBool ("Run", false);
+				anim.SetInteger ("Transicion", 1);
+
+				Vector3 targetPosition = new Vector3 (playerTarget.position.x, transform.position.y,
+					playerTarget.position.z);
+
+				transform.rotation = Quaternion.Slerp (transform.rotation,
+					Quaternion.LookRotation (targetPosition - transform.position), 5f * Time.deltaTime);
+
+				if (currentAttackTime >= waitAttackTime) {
+
+					int atkRange = Random.Range (1, 5);
+					anim.SetInteger ("Transicion", 2);
+
+					currentAttackTime = 0f;
+					finishedAttacking = false;
+				} else {
+					anim.SetInteger ("Transicion", 1);
+					currentAttackTime += Time.deltaTime;
+				}
+
+			} else {
+				anim.SetInteger ("Transicion", 0);
+				navAgent.isStopped = true;
+			}
+
+		} 
+
+	}
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
